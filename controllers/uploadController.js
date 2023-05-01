b9d3472bf7364bbb64b1d1140131ff71769a1bdb
fraidyEfrat <36214264632@mby.co.il>
2023-05-01 16:28:29 +0300

    route public

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -5,24 +5,23 @@ const upload = async (req, res) =>{
     if(!req.file){
         res.status(500).send("No file")
     }
+    console.log("in uploud controler")
     try{
     const file = req.file
     const folder = path.join(__dirname, "..", "public", "images")
     const filename = `${uuid()}_${req.file.originalname}`
     const fileUrl  =`${folder}/${filename}`
-    }catch(err){
-    res.status(501).send(err)
-}
-
-    try{
+    
+        
         await fsPromises.writeFile(fileUrl, req.file.buffer)
         return res.json({location: fileUrl, name:filename })
+
     }catch(err){
-        res.status(500).send(err)
+        res.status(505).send(err)
 
     }
-
-    
+ res.send("test")
+   
 }
 
 module.exports = {upload}
